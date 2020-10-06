bd4b30f065a68b12c1399e2efefbd7121150d797
Elena <sir.elena.lvaose@gmail.com>
2020-10-06 13:08:54 -0400

    create table statements

--- src/db_migrate.py
+++ src/db_migrate.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
                             port="5432")
     try:
         cur = conn.cursor()
+
         cur.execute("""
                     CREATE TABLE IF NOT EXISTS users_table(
                         id SERIAL NOT NULL PRIMARY KEY,
@@ -17,6 +18,32 @@ if __name__ == '__main__':
                         password VARCHAR ( 50 ) NOT NULL,
                         email VARCHAR ( 255 ) UNIQUE NOT NULL,
                         created_on TIMESTAMP NOT NULL
+                    )
+                    """)
+
+        cur.execute("""
+                    CREATE TABLE IF NOT EXISTS raw_data(
+                        user_id INTEGER NOT NULL
+                                REFERENCES users_table (id),
+                        ts TIMESTAMP NOT NULL,
+                        gsr_reading DOUBLE NOT NULL
+                    )
+                    """)
+
+        cur.execute("""
+                    CREATE TABLE IF NOT EXISTS processed_data(
+                        user_id INTEGER NOT NULL
+                                REFERENCES users_table (id),
+                        ts TIMESTAMP NOT NULL,
+                        state VARCHAR ( 50 ) NOT NULL
+                    )
+                    """)
+
+        cur.execute("""
+                    CREATE TABLE IF NOT EXISTS latest_received(
+                        user_id INTEGER NOT NULL
+                                REFERENCES users_table (id),
+                        latest_received_ts TIMESTAMP NOT NULL
                     )
                     """)
 
