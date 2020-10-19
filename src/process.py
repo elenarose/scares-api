4f26927b1aa2555fbb381cc276d267e3170fda03
Elena <sir.elena.lvaose@gmail.com>
2020-10-19 13:35:45 -0400

    comments

--- src/process.py
+++ src/process.py
@@ -7,7 +7,7 @@ def unpack_message(msg):
 
 def run():
     i = 0
-    max_msg_count = 3
+    max_msg_count = 1 #number of messages to pull from queue at a time
     while i < 100:
         #read messages from the sqs queue
         messages = receive_messages('new_data', max_msg_count, 2)
@@ -15,7 +15,10 @@ def run():
             for msg in messages:
                 body = unpack_message(msg)
                 raw_data = get_gsr_values(body.user_id, body.ts - 5, body.ts)
-                #do math with raw_data
+                #TODO do math with raw_data
+                # feature extraction and classification happens here for a segment of data
+                #TODO
+                # write the result of classification to the database
         sleep(2)
         i = i + 1
 
