a6c697c04b5b435b79b8c0bc32c04d004334cba3
Elena <sir.elena.lvaose@gmail.com>
2020-11-04 13:32:32 -0500

    use a config file to get db credentials from the environment variables

---
+++ src/config.py
@@ -0,0 +1,10 @@
+"""Load config from environment variables."""
+from os import environ
+
+class Config:
+    # Database config
+    PG_HOST = environ.get('DATABASE_HOST','localhost')
+    PG_USER = environ.get('DATABASE_USERNAME','postgres')
+    PG_PASSWORD = environ.get('DATABASE_PASSWORD','postgres')
+    PG_PORT = environ.get('DATABASE_PORT','5432')
+    PG_NAME = environ.get('DATABASE_NAME','scares')
