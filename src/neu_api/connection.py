9d710cc02da40b9265e3197342e71b29878314fd
Darin Hunt <darinhunt@Darins-MacBook-Pro-2.local>
2020-11-16 11:37:40 -0500

    adding correction to script

--- src/neu_api/connection.py
+++ src/neu_api/connection.py
@@ -3,14 +3,15 @@ import time
 from datetime import datetime
 import requests
 
-def connect():
+def connect(device_id, address, port):
     while True:
 
         try:
             req = requests.get("http://{}:{}/NeuLogAPI?GetSensorValue:[GSR],[{}]".format(address, port, device_id))
 
-        except ConnectionError:
+        except:
             print("Not Connected to API...Trying Again...")
+            time.sleep(5)
             continue
 
         value = json.loads(req.text)
@@ -20,12 +21,13 @@ def connect():
         current_time = now.strftime("%H:%M:%S")
 
         post_call = "curl http://localhost:8080/data?\?user_id\={}\&value={}\&time={}".format(device_id, value, current_time)
+        os.system(post_call)
 
         time.sleep(5)
 
 if __name__ == '__main__':
 
- device_id = input("Enter a User ID: ")
+ device_id = input("Enter a device ID: ")
  address = input("Enter the address of API: ")
  port = input("Enter the port of the API: ")
- connect(device_id)
+ connect(device_id, address, port)
