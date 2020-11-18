2406ad2d10a6bb2a2c2db241266fb9e34ed43ee2
Darin Hunt <darinhunt@Darins-MacBook-Pro-2.local>
2020-11-17 22:17:32 -0500

    adding timing script

--- src/neu_api/connection.py
+++ src/neu_api/connection.py
@@ -10,17 +10,14 @@ def connect(device_id, neu_address, neu_port, user_id=1):
     header = {'Authorization': 'E!&3F3ugu;Pvnc6E'}
     user_param = {"user_id":user_id}
 
-
     while True:
 
         timestamps = []
         gsr_values = []
 
-        for i in range(5):
-
+        for _ in range(100):
             try:
                 req = requests.get("http://{}:{}/NeuLogAPI?GetSensorValue:[GSR],[{}]".format(neu_address, neu_port, device_id))
-
             except:
                 print("Not Connected to API...Trying Again...")
                 time.sleep(5)
@@ -35,10 +32,11 @@ def connect(device_id, neu_address, neu_port, user_id=1):
             timestamps.append(current_time)
             gsr_values.append(value)
 
-            data_post = {"timestamps": timestamps, "gsr_values": gsr_values}
+            time.sleep(0.005)
 
+        data_post = {"timestamps": timestamps, "gsr_values": gsr_values}
         requests.post(aws_address, json=data_post, headers=header, params=user_param)
-        #time.sleep(5)
+
 
 if __name__ == '__main__':
 
