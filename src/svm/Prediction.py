e9b239a01eaf46e69a641ade8b29a7dd03fd1bb4
Elena <sir.elena.lvaose@gmail.com>
2020-11-19 19:19:00 -0500

    dont need this file

--- src/svm/Prediction.py
+++
@@ -1,9 +0,0 @@
-from SVM import classifier_linear
-from SVM import y_pred_linear
-from SVM import get_prediction
-from SVM import data
-#code to get live data  from a file name
-import pandas as pd
-
-prediction_data = pd.array()
-get_prediction(prediction_data)
